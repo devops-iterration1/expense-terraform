57d7e8d2f0762a578becfadced5add4bd2719980
izypsy <reddy.nie11@gmail.com>
2024-04-16 20:57:17 +0530

    testing the vault with 1 service

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 # entry point of terraform project.
-module "frontend" {
-  source = "./modules/app"
-  instance_type = var.instance_type
-  component = "frontend"
-  env = var.env
-  zone_id = var.zone_id
-  depends_on = [module.backend]
-}
-module "backend" {
-  source = "./modules/app"
-  instance_type = var.instance_type
-  component = "backend"
-  env = var.env
-  zone_id = var.zone_id
-  depends_on = [module.database]
-}
+#module "frontend" {
+#  source = "./modules/app"
+#  instance_type = var.instance_type
+#  component = "frontend"
+#  env = var.env
+#  zone_id = var.zone_id
+#  depends_on = [module.backend]
+#}
+#module "backend" {
+#  source = "./modules/app"
+#  instance_type = var.instance_type
+#  component = "backend"
+#  env = var.env
+#  zone_id = var.zone_id
+#  depends_on = [module.database]
+#}
 module "database" {
   source = "./modules/app"
   instance_type = var.instance_type
