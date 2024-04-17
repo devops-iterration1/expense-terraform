0e1e1584c1bb4c94750363f09c4301000b4dd207
izypsy <reddy.nie11@gmail.com>
2024-04-17 15:51:41 +0530

    testing the depends_on

--- main.tf
+++ main.tf
@@ -1,21 +1,23 @@
 # entry point of terraform project.
 module "frontend" {
+  depends_on = [module.backend]
+
   source = "./modules/app"
   instance_type = var.instance_type
   component = "frontend"
   env = var.env
   zone_id = var.zone_id
   vault_token = var.vault_token
-  depends_on = [module.backend]
 }
 module "backend" {
+  depends_on = [module.database]
+
   source = "./modules/app"
   instance_type = var.instance_type
   component = "backend"
   env = var.env
   zone_id = var.zone_id
   vault_token = var.vault_token
-  depends_on = [module.database]
 }
 module "database" {
   source = "./modules/app"
