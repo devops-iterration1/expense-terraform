3c3fe6c80bce8f159d683ed0d196045329638295
izypsy <reddy.nie11@gmail.com>
2024-04-17 15:07:53 +0530

    remove secrets after ansible execution

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -20,7 +20,7 @@ resource "null_resource" "provision_ansible" {
     inline = [
       "sudo pip install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/devops-iterration1/expense-ansible get-secrets.yml -e vault_token=${var.vault_token} -e env=${var.env} -e role_name=${var.component}",
-      "ansible-pull -i localhost, -U https://github.com/devops-iterration1/expense-ansible expense-setup.yml -e env=${var.env} -e role_name=${var.component} -e @ssh-secrets.json -e @app-secrets.json",
+      "ansible-pull -i localhost, -U https://github.com/devops-iterration1/expense-ansible expense-setup.yml -e env=${var.env} -e role_name=${var.component} -e @~/ssh-secrets.json -e @~/app-secrets.json",
       "rm -f ~/ssh-secrets.json ~/app-secrets.json"
     ]
   }
