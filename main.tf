ded3640d0c35f21e250a0b29983a109d5a59ad14
izypsy <reddy.nie11@gmail.com>
2024-05-08 21:54:48 +0530

    enable app modules in create

--- main.tf
+++ main.tf
@@ -1,24 +1,28 @@
 ## entry point of terraform project.
-#module "frontend" {
-#  depends_on = [module.backend]
-#
-#  source = "./modules/app"
-#  instance_type = var.instance_type
-#  component = "frontend"
-#  env = var.env
-#  zone_id = var.zone_id
-#  vault_token = var.vault_token
-#}
-#module "backend" {
-#  depends_on = [module.database]
-#
-#  source = "./modules/app"
-#  instance_type = var.instance_type
-#  component = "backend"
-#  env = var.env
-#  zone_id = var.zone_id
-#  vault_token = var.vault_token
-#}
+module "frontend" {
+  depends_on = [module.backend]
+
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "frontend"
+  env = var.env
+  zone_id = var.zone_id
+  vault_token = var.vault_token
+  vpc_id = module.vpc.vps_id
+  subnets = module.vpc.fe_subnets
+}
+module "backend" {
+  depends_on = [module.database]
+
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "backend"
+  env = var.env
+  zone_id = var.zone_id
+  vault_token = var.vault_token
+  vpc_id = module.vpc.vps_id
+  subnets = module.vpc.be_subnets
+}
 module "database" {
   source = "./modules/app"
   instance_type = var.instance_type
