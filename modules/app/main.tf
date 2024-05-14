91e6725e77ecf8654fc40d8206548306e4c58f78
izypsy <reddy.nie11@gmail.com>
2024-05-15 00:57:35 +0530

    adding health checks to alb_tg

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -97,6 +97,16 @@ resource "aws_lb_target_group" "main_tg" {
   port = var.app_port
   protocol = "HTTP"
   vpc_id = var.vpc_id
+  deregistration_delay = 15
+
+  health_check {
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    port                = var.app_port
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
 }
 
 resource "aws_lb_target_group_attachment" "main_tg_att" {
